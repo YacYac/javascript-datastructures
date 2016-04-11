1a5b1160456893c40b7761036d8326e201ec810b
Blair Yacishyn <atezun@gmail.com>
2016-04-11 06:06:19 +0000

    added doubly linked lists

---
+++ chapter05-doubly_linked_lists.js
@@ -0,0 +1,13 @@
+function DoublyLinkedList() {
+  var Node = function (element) {
+    this.element;
+    this.next;
+    this.prev; //NEW
+  };
+
+  var length = 0;
+  var head = null;
+  var tail = null; //NEW
+
+  //methods here
+}
