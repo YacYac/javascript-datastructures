b62d5e020527ed1294da03be634515fe1f4ddd08
Blair Yacishyn <atezun@gmail.com>
2016-04-11 04:36:14 +0000

    added linked lists file

---
+++ chapter05-linked_list.js
@@ -0,0 +1,19 @@
+function LinkedList() {
+  var Node = function (element) { //{1}
+    this.element = element;
+    this.next = null;
+  };
+
+  var length = 0;
+  var head = null;
+
+  this.append = function (element) {};
+  this.insert = function (position, element) {};
+  this.removeAt = function (position) {};
+  this.remove = function (element) {};
+  this.indexOf = function (element) {};
+  this.isEmpty = function () {};
+  this.size = function () {};
+  this.toString = function () {};
+  this.print = function () {};
+}
